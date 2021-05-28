5788806605abd080deecbe49700bf5c6bf489896
chinomso <posh.bc@gmail.com>
2021-05-28 16:15:42 +0100

    Add README as well as initial index and schema

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,8 @@
 require('dotenv').config();
+
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({ typeDefs });
+
+
---
+++ start/server/src/schema.js
@@ -0,0 +1,8 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  # Your schema will go here
+`;
+
+module.exports = typeDefs;
+
